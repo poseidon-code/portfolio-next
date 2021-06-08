f79d5334ce23ac08c0ac2daf3c5b4e3d0549d2ef
poseidon-code <pritamhalder05062000@gmail.com>
2021-06-08 18:54:23 +0530

    create: route /

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import styles from '../styles/Home.module.scss';
+import Page from '../components/Page/Page';
+
+const Home = () => {
+    return (
+        <Page background='backgrounds/bg-1.webm' page='Portfolio'>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. In non quod nobis maiores! Fuga obcaecati recusandae vitae laboriosam,
+            accusantium quos eveniet enim delectus praesentium voluptate quibusdam labore, vero adipisci deleniti.
+        </Page>
+    );
+};
+
+export default Home;
